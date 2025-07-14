0b9aa8242295dd97f418945020a4d32bf75f9496
rayjunevx <rayjunevx@gmail.com>
2025-07-15 05:46:05 +0700

    Update list_apis.php

--- api/list_apis.php
+++ api/list_apis.php
@@ -1,11 +1,13 @@
 <?php
 require '../config.php';
-$data = ["apis" => [], "parse_url" => ""];
+$data = ["apis" => [], "parse_url" => "https://your-parse-url.com/parse?url="]; // 直接使用默认解析地址
+
+// 获取视频API列表
 $res = $conn->query("SELECT * FROM video_apis ORDER BY id DESC");
 while ($row = $res->fetch_assoc()) {
   $data['apis'][] = ["id" => $row['id'], "name" => $row['name'], "api_url" => $row['api_url']];
 }
-$parse = $conn->query("SELECT value FROM config WHERE key_name='player_parse_url'");
-if ($row = $parse->fetch_assoc()) $data['parse_url'] = $row['value'];
+
+
 echo json_encode($data);
 ?>
